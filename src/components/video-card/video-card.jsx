1ebeb5a4d1832e010ab9b352225dbe274386013e
mukhammadjondev <muhsdev@gmail.com>
2023-08-09 19:50:23 +0500

    Add video detail page

--- src/components/video-card/video-card.jsx
+++ src/components/video-card/video-card.jsx
@@ -2,13 +2,16 @@ import moment from "moment/moment"
 import { CheckCircle } from "@mui/icons-material"
 import { Avatar, Card, CardContent, CardMedia, Typography, Stack } from "@mui/material"
 import { colors } from "../../constants/colors"
+import { Link } from "react-router-dom"
 
 const VideoCard = ({video}) => {
   return (
     <Card sx={{width: '100%', boxShadow: 'none', borderRadius: 0}}>
+      <Link to={`/video/${video.id.videoId}`}>
       <CardMedia image={video?.snippet?.thumbnails?.high?.url} alt={video?.snippet?.title} sx={{width: '100%', height: '210px'}} />
+      </Link>
       <CardContent sx={{background: colors.primary, height: '200px', position: 'relative'}}>
-        <>
+        <Link to={`/video/${video.id.videoId}`}>
           <Typography my={'5px'} sx={{opacity: '.4'}}>
             {moment(video?.snippet?.publishedAt).fromNow()}
           </Typography>
@@ -18,7 +21,7 @@ const VideoCard = ({video}) => {
           <Typography variant="subtitle2" sx={{opacity: '.6'}}>
             {video?.snippet?.description.slice(0, 70)}
           </Typography>
-        </>
+        </Link>
         <>
           <Stack direction={'row'} position={'absolute'} bottom={'10px'} alignItems={'center'} gap={'5px'}>
             <Avatar src={video?.snippet?.thumbnails?.default?.url} />
--- src/components/video-detail/video-detail.jsx
+++ src/components/video-detail/video-detail.jsx
@@ -1,6 +1,75 @@
+import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { ApiServise } from '../../service/api.servise'
+import { Avatar, Box, Button, Chip, Container, Stack, Typography } from "@mui/material"
+import ReactPlayer from "react-player"
+import { CheckCircle, FavoriteOutlined, MarkChatRead, Tag, Visibility } from "@mui/icons-material"
+import { Loader, Videos } from '../'
+
 const VideoDetail = () => {
+  const {id} = useParams()
+  const [videoDetail, setVideoDetail] = useState('')
+  const [relatedVideo, setRelatedVideo] = useState('')
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await ApiServise.fetching(`videos?part=snippet,statistics&id=${id}`)
+      setVideoDetail(data.items[0])
+      const relatedData = await ApiServise.fetching(`search?part=snippet&relatedToVideoId=${id}&type=video`)
+      setRelatedVideo(relatedData.items)
+    }
+    getData()
+  }, [id])
+  if (!videoDetail.snippet) return <Loader />
+
+  const {
+    snippet: {tags, title, channelId, channelTitle, description, thumbnails},
+    statistics: {viewCount, likeCount, commentCount}
+  } = videoDetail
+
   return (
-    <div>VideoDetail</div>
+    <Box minHeight={'90vh'} mb={10}>
+      <Container sx={{display: 'flex', flexDirection: {xs: 'column', md: 'row'}}}>
+        <Box width={{xs: '100%', md: '70%'}}>
+          <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className='react-player' controls />
+          {tags.map((item, idx) => (
+            <Chip label={item} key={idx} sx={{marginTop: '10px', cursor: 'pointer', ml: '10px'}} deleteIcon={<Tag />} onDelete={() => {}} variant='outlined' />
+          ))}
+          <Typography variant="h5" fontWeight='bold' p={2}>
+            {title}
+          </Typography>
+          <Typography variant="subtitle2" p={2} sx={{opacity: '.7'}}>
+            {description.slice(0, 700)} ...more
+          </Typography>
+          <Stack direction='row' gap='20px' alignItems='center' py={1} px={2}>
+            <Stack sx={{opacity: 0.7}} direction='row' alignItems='center' gap='3px'>
+              <Visibility />
+              {parseInt(viewCount).toLocaleString()} views
+            </Stack>
+            <Stack sx={{opacity: 0.7}} direction='row' alignItems='center' gap='3px'>
+              <FavoriteOutlined />
+              {parseInt(likeCount).toLocaleString()} likes
+            </Stack>
+            <Stack sx={{opacity: 0.7}} direction='row' alignItems='center' gap='3px'>
+              <MarkChatRead />
+              {parseInt(commentCount).toLocaleString()} comment
+            </Stack>
+          </Stack>
+          <Stack direction='row' py={1} px={2}>
+            <Stack direction='row' alignItems='center' gap='5px' marginTop='5px'>
+              <Avatar src={thumbnails?.default?.url} alt={channelTitle} />
+              <Typography variant='subtitle2' color='gray'>
+                {channelTitle}
+                <CheckCircle sx={{fontSize: '12px', color: 'gray', ml: '5px'}} />
+              </Typography>
+            </Stack>
+          </Stack>
+        </Box>
+        <Box width={{xs: '100%', md: '30%'}} sx={{paddingTop: {xs: '40px', md: '0px'}, paddingLeft: {xs: '0px', md: '20px', justifyContent: 'center', alignItems: 'center', overflow: 'scroll', maxHeight: '150vh'}}}>
+          <Videos videos={relatedVideo} />
+        </Box>
+      </Container>
+    </Box>
   )
 }
 
